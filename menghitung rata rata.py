9e4eab1477355ccdbc51941c07fd20d25e6a6e83
Pitrimanalu <manaluf39@gmail.com>
2026-01-11 16:00:20 +0700

    Add files via upload

---
+++ menghitung rata rata.py
@@ -0,0 +1,16 @@
+# Program menghitung rata rata nilai ujian matematika,kimia,dan Fisika
+
+nilai_matematika = float(input("masukkan nilai matematika:  "))
+
+nilai_kimia = float(input("masukkan nilai kimia:  "))
+
+nilai_fisika = float(input("masukkan nilai fisika:  "))
+
+#Menghitung nilai rata rata
+rata_rata =(nilai_matematika+nilai
+            -kimia+nilai_fisika) / 3
+
+#menampilkan hasil rata rata
+print("f"nilai rata rata adalah
+      {round(rata_rata)},")
+
